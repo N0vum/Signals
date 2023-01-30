f0838e0fd706255e7f2c2b72a90529205aa5b392
Elena <n0vum@yandex.ru>
2023-01-30 13:27:10 +0700

    created some foolish code

---
+++ go.sh
@@ -0,0 +1,29 @@
+let "s=0"
+let "i=1"
+sudo apt-get install fim
+if [[ -z fim ]] 
+then exit
+else
+echo "Введите названия сигналов"
+sleep 1
+while [[ $i -le 9 ]]
+do
+fim $i.jpg &
+jpg_pid=$!
+sleep 1
+kill $jpg_pid
+read answer
+for key in `cat keys.txt`
+do
+if [[ key==answer ]]
+then let "s++"
+fi
+done
+let "i++"
+done
+echo $s
+sudo apt-get remove fim
+fi
+	
+
+
