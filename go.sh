ab455ae3918cb443615591398fa8ece4b5f10496
Elena Kazakova <61557802+N0vum@users.noreply.github.com>
2023-01-30 16:52:19 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,29 +1,23 @@
 let "s=0"
-let "i=1"
 sudo apt-get install fim
-if [[ -z `whereis fim` ]] 
+if [[ -z `whereis fim` ]]
 then exit
 else
 echo "Введите названия сигналов"
 sleep 1
-while [[ $i -le 9 ]]
+for i in $(seq 1 9);
 do
 fim $i.jpg &
 jpg_pid=$!
 sleep 1
 kill $jpg_pid
 read answer
-for key in `cat keys.txt`
-do
-if [[ key==answer ]]
+key=`head -n ${i} keys.txt | grep $answer`
+if [[ !(-z $key) ]]
 then let "s++"
 fi
-done
 let "i++"
 done
 echo "У вас $s верных ответов из 9"
 sudo apt-get remove fim
 fi
-	
-
-
