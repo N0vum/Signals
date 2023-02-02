976bbafb4eb8324f6ec11c4d180056608177e607
Elena <n0vum@yandex.ru>
2023-02-02 21:51:33 +0700

    updated

--- go.sh
+++ go.sh
@@ -1,23 +1,27 @@
-let "s=0"
+flash_pic(){
+	fim "$1.jpg" &
+	jpg_pid=$!
+	sleep 1
+	kill $jpg_pid
+}
 sudo apt-get install fim
-if [[ -z `whereis fim` ]]
-then exit
-else
-echo "Введите названия сигналов"
+[[ -z `whereis fim` ]] && exit
+echo "Введите названия сигналов:"
 sleep 1
+correct=0
 for i in $(seq 9);
 do
-fim $i.jpg &
-jpg_pid=$!
-sleep 1
-kill $jpg_pid
+flash_pic "$i"	
 read answer
+answer=$(echo "$answer" | sed -e "s/^[[:space:]]*//")
 key=`head -n ${i} keys.txt | grep -i "\b${answer}\b"`
-if [[ !(-z $key) && !(-z $answer)]]
-then let "s++"
-fi
-let "i++"
+[[ !(-z $key) && !(-z $answer)]] && ((correct=correct+1))
 done
-echo "У вас $s верных ответов из 9"
-sudo apt-get remove fim
-fi
+result(){
+echo "У вас $correct верны$1 ответ$2 из 9"
+}
+case $correct in
+2|3|4) result "x" "a";;
+1) result "й" "";;
+*) result "х" "ов"
+esac	
